e8c82a5d955050f1f9f1a2faa0ff1db095cb8abb
Joe Dallago <joed@surveymonkey.com>
2012-07-10 10:16:36 -0700

    Added a fallback for if the README/CHANGES aren't located in the distribution folder.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
+try:
+    README = open(os.path.join(here, 'README.md')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
+except IOError:
+    CHANGES = README = ''
 
 requires = [
 ]
